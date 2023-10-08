97834fe150165780982c08f1b2827bd8a83240f2
sina <mubarriizz@gmail.com>
2023-10-09 01:03:45 +0330

    first commit

---
+++ client.py
@@ -0,0 +1,31 @@
+import socketio
+
+# Create a Socket.IO client instance
+sio = socketio.Client()
+
+# Define an event handler for the 'connect' event
+@sio.event
+def connect():
+    print('Connected to server')
+
+# Define an event handler for the 'response' event
+@sio.event
+def response(data):
+    print('Received response:', data)
+
+# Define an event handler for the 'disconnect' event
+@sio.event
+def disconnect():
+    print('Disconnected from server')
+
+if __name__ == '__main__':
+    # Connect to the Socket.IO server
+    sio.connect('http://localhost:8000')
+
+    # Send a message to the server for example name of city
+    while True:
+        user_input = input('city name : ')
+        sio.emit('message', user_input)
+
+        # Wait for events indefinitely
+        sio.wait()
---
+++ server.py
@@ -0,0 +1,35 @@
+import socketio
+from gevent import pywsgi
+import requests
+# from geventwebsocket.handler import WebSocketHandler
+
+
+
+# Create a Socket.IO server instance
+sio = socketio.Server()
+
+# Define an event handler for the 'connect' event
+@sio.on('connect')
+def connect(sid, environ):
+    print('Client connected:', sid)
+
+# Define an event handler for the 'message' event
+@sio.on('message')
+def message(sid, data):
+
+    # response = requests.get('api url to get wheather data')
+    
+    response = data
+    sio.emit('response', {'message':response }, room=sid)
+
+# Define an event handler for the 'disconnect' event
+@sio.on('disconnect')
+def disconnect(sid):
+    print('Client disconnected:', sid)
+
+if __name__ == '__main__':
+    # Wrap the Socket.IO server with a WSGI application
+    app = socketio.WSGIApp(sio)
+
+    # Run the server on localhost:5000
+    pywsgi.WSGIServer(('localhost', 8000), app).serve_forever()
